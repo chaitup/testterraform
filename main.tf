f5c502e8b516e5b748d5b815bbd1dd55615436d9
chaitup <cpchmpll@memphis.edu>
2017-05-16 15:38:52 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
    backend "s3" {
      bucket = "chaitujenkinsdcos"
-     key    = "terraform.tfstate"
+     key    = "tf.tfstate"
      region = "us-east-1"
    }
  }
